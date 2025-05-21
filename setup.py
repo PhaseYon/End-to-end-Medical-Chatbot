7673a223aa16d93301407fa1e7c4d249ec362fe7
Fezan Ahmed <crescent4prez@gmail.com>
2025-05-21 19:50:11 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="E2E Medican Chatbot",
+    version='0.0.0',
+    author='Fezan Ahmed',
+    author_email='ahmedfezan58@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
